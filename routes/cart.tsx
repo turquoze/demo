a9fc936cb3c993d5e521f2e9abeb765ae5a6e426
EduM22 <38257387+EduM22@users.noreply.github.com>
2022-05-09 20:14:54 +0200

    add new pages

---
+++ routes/cart.tsx
@@ -0,0 +1,12 @@
+/** @jsx h */
+import { h } from "../client_deps.ts";
+
+export default function Cart() {
+  return (
+    <div>
+      <p>
+        Cart
+      </p>
+    </div>
+  );
+}
---
+++ routes/products/index.tsx
@@ -0,0 +1,12 @@
+/** @jsx h */
+import { h } from "../../client_deps.ts";
+
+export default function Products() {
+  return (
+    <div>
+      <p>
+        Products
+      </p>
+    </div>
+  );
+}
