c2dfcf503cd887a1a871096757c5b251c6a87f8c
EduM22 <38257387+EduM22@users.noreply.github.com>
2022-10-26 20:22:43 +0200

    fix checked state on filters

--- islands/Filters.tsx
+++ islands/Filters.tsx
@@ -1,11 +1,69 @@
 import { Product, SearchInfo, UsedFilter } from "../utils/types.ts";
+import { useEffect, useState } from "preact/hooks";
 
 interface FiltersProps {
   info: SearchInfo;
-  usedFilters: UsedFilter;
+  // deno-lint-ignore ban-types
+  onFilter: Function;
 }
 
 export default function Filters(props: FiltersProps) {
+  const [usedFilters, setUsedFilters] = useState<Record<string, number>>({});
+
+  useEffect(() => {
+    const url = new URL(window.location.toString());
+    const filters = url.searchParams.get("filters");
+
+    if (filters != null) {
+      const usedFiltersLocal = JSON.parse(filters);
+      setUsedFilters(usedFiltersLocal);
+    } else {
+      setUsedFilters({});
+    }
+  }, [props]);
+
+  const handleClick = (e: Event) => {
+    //@ts-expect-error not on type
+    const id = e.target.id;
+    //@ts-expect-error not on type
+    const value = e.target.value;
+
+    const filters = getFilters();
+    const entry = Object.keys(filters).find((x) => x == id);
+    if (entry != undefined) {
+      delete filters[id];
+    } else {
+      filters[id] = value;
+    }
+
+    setUrl(filters);
+    props.onFilter();
+  };
+
+  function setUrl(filter: Record<string, number>) {
+    const url = new URL(window.location.toString());
+
+    if (Object.keys(filter).length === 0) {
+      url.searchParams.delete("filters");
+    } else {
+      url.searchParams.set("filters", JSON.stringify(filter));
+    }
+
+    window.history.replaceState({}, document.title, url);
+  }
+
+  function getFilters(): Record<string, number> {
+    const url = new URL(window.location.toString());
+
+    const filters = url.searchParams.get("filters");
+
+    if (filters == null) {
+      return {};
+    }
+
+    return JSON.parse(filters);
+  }
+
   return (
     <>
       <form>
@@ -18,11 +76,10 @@ export default function Filters(props: FiltersProps) {
                   <h4 class="pb-2">{key}</h4>
                   <div class="space-y-4">
                     {Object.entries(values).map(([value, amount]) => {
-                      /*const match = props.usedFilters.find((x) =>
-                        x.id == filter && x.value == option
-                      );*/
-                      const match = undefined;
                       const htmlId = `filter-${key}-${value}`;
+                      const match = Object.keys(usedFilters).find((x) =>
+                        x == htmlId
+                      );
                       return (
                         <div key={htmlId} class="flex items-center">
                           <input
@@ -30,6 +87,7 @@ export default function Filters(props: FiltersProps) {
                             id={htmlId}
                             name={value}
                             value={value}
+                            onChange={handleClick}
                             type="checkbox"
                             class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
                           />
--- islands/SearchForm.tsx
+++ islands/SearchForm.tsx
@@ -1,6 +1,7 @@
 import { useState } from "preact/hooks";
 import ProductCard from "../components/ProductCard.tsx";
 import { Product, SearchProps } from "../utils/types.ts";
+import Filters from "./Filters.tsx";
 
 export default function SearchForm(props: SearchProps) {
   const [products, setProducts] = useState(props.products);
@@ -23,7 +24,12 @@ export default function SearchForm(props: SearchProps) {
   }
 
   async function search(offset?: number, localLimit = props.limit) {
-    let url = `/search?q=${query}&limit=${localLimit}`;
+    const getFilters = getSearchFilters();
+    const filters = Array.from(getFilters.values());
+    const filterString = filters.join(" AND ");
+
+    let url = `/search?q=${query}&limit=${localLimit}&filter="${filterString}"`;
+
     if (offset != null) {
       url = url + `&offset=${offset}`;
     }
@@ -86,78 +92,113 @@ export default function SearchForm(props: SearchProps) {
     await search();
   }
 
+  function getSearchFilters() {
+    const url = new URL(window.location.toString());
+    const filters = url.searchParams.get("filters");
+
+    const usedFilter = new Map<string, string>();
+
+    if (filters != null) {
+      const obj: Record<string, number> = JSON.parse(filters);
+      Object.entries(obj).map(([key, value]) => {
+        const filterId = key.split("-")[1];
+
+        if (usedFilter.has(filterId)) {
+          const oldVal = usedFilter.get(filterId);
+
+          const newVal = oldVal + " OR " + filterId + " = " + value.toString();
+          usedFilter.set(filterId, newVal);
+        } else {
+          usedFilter.set(filterId, filterId + " = " + value.toString());
+        }
+      });
+    }
+
+    return usedFilter;
+  }
+
   return (
     <>
-      <div class="my-8 flex justify-center">
-        <form
-          onSubmit={onSubmit}
-          method="GET"
-          action="/search"
-        >
-          <div class="grid grid-cols-3 justify-items-center">
-            <div class="col-span-2">
-              <input
-                type="text"
-                name="q"
-                class="h-10 block w-full pl-7 text-sm pr-12 border-gray-300 rounded-md shadow-md border"
-                placeholder="search"
-                value={query}
-                onInput={onValueChange}
-                id="search-input"
-              />
-            </div>
+      <div class="grid grid-cols-1 md:grid-cols-3">
+        <div class="p-4">
+          <Filters
+            info={props.info}
+            onFilter={search}
+          />
+        </div>
+        <div class="col-span-2">
+          <div class="my-8 flex justify-center">
+            <form
+              onSubmit={onSubmit}
+              method="GET"
+              action="/search"
+            >
+              <div class="grid grid-cols-3 justify-items-center">
+                <div class="col-span-2">
+                  <input
+                    type="text"
+                    name="q"
+                    class="h-10 block w-full pl-7 text-sm pr-12 border-gray-300 rounded-md shadow-md border"
+                    placeholder="search"
+                    value={query}
+                    onInput={onValueChange}
+                    id="search-input"
+                  />
+                </div>
 
-            <div class="col-span-1">
-              <button
-                class="w-28 bg-black rounded-md h-full ml-2 px-8 flex items-center justify-center text-base text-white focus:outline-none focus:ring-2 focus:ring-offset-2"
-                type="submit"
-              >
-                Search
-              </button>
-            </div>
+                <div class="col-span-1">
+                  <button
+                    class="w-28 bg-black rounded-md h-full ml-2 px-8 flex items-center justify-center text-base text-white focus:outline-none focus:ring-2 focus:ring-offset-2"
+                    type="submit"
+                  >
+                    Search
+                  </button>
+                </div>
+              </div>
+            </form>
           </div>
-        </form>
-      </div>
-      <div class="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-        {products?.map((product: Product) => {
-          return <ProductCard product={product} />;
-        })}
-        {props.hits == 0 && products.length == 0
-          ? (
-            <h2 class="text-4xl tracking-tight text-gray-900 mb-2">
-              No search hits
-            </h2>
-          )
-          : null}
-      </div>
-      <div class="flex justify-center pt-10">
-        <h5>hits - {hits}</h5>
-      </div>
-      <div class="flex justify-center pt-2">
-        {stateOffset > 0 && stateOffset <= hits + props.limit
-          ? (
-            <a
-              href={prevServerSideUrl}
-              onClick={handleLoadPrevData}
-              class="w-28 m-4 pt-2 pb-2 bg-black rounded-md h-full ml-2 px-8 flex items-center justify-center text-base text-white focus:outline-none focus:ring-2 focus:ring-offset-2"
-              type="submit"
-            >
-              Back
-            </a>
-          )
-          : null}
-        {stateOffset + props.limit < hits
-          ? (
-            <a
-              href={nextServerSideUrl}
-              onClick={handleLoadMoreData}
-              class="w-28 m-4 pt-2 pb-2 bg-black rounded-md h-full ml-2 px-8 flex items-center justify-center text-base text-white focus:outline-none focus:ring-2 focus:ring-offset-2"
-              type="submit"
-            >
-              Next
-            </a>
-          )
-          : null}
+          <div class="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+            {products?.map((product: Product) => {
+              return <ProductCard product={product} />;
+            })}
+            {props.hits == 0 && products.length == 0
+              ? (
+                <h2 class="text-4xl tracking-tight text-gray-900 mb-2">
+                  No search hits
+                </h2>
+              )
+              : null}
+          </div>
+          <div class="flex justify-center pt-10">
+            <h5>hits - {hits}</h5>
+          </div>
+          <div class="flex justify-center pt-2">
+            {stateOffset > 0 && stateOffset <= hits + props.limit
+              ? (
+                <a
+                  href={prevServerSideUrl}
+                  onClick={handleLoadPrevData}
+                  class="w-28 m-4 pt-2 pb-2 bg-black rounded-md h-full ml-2 px-8 flex items-center justify-center text-base text-white focus:outline-none focus:ring-2 focus:ring-offset-2"
+                  type="submit"
+                >
+                  Back
+                </a>
+              )
+              : null}
+            {stateOffset + props.limit < hits
+              ? (
+                <a
+                  href={nextServerSideUrl}
+                  onClick={handleLoadMoreData}
+                  class="w-28 m-4 pt-2 pb-2 bg-black rounded-md h-full ml-2 px-8 flex items-center justify-center text-base text-white focus:outline-none focus:ring-2 focus:ring-offset-2"
+                  type="submit"
+                >
+                  Next
+                </a>
+              )
+              : null}
+          </div>
+        </div>
       </div>
     </>
   );
